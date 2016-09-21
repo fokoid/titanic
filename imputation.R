7ef943ce3dd87a52d93a967e76b5d4bafbd69f25
Alex Thorne <alex@alexthorne.net>
2016-09-21 16:22:04 +0100

    Style changes and minor refactoring

--- imputation.R
+++ imputation.R
@@ -7,29 +7,34 @@ library('rattle')
 library('rpart.plot')
 library('RColorBrewer')
 
-pdfPlot <- function(x, name) {
-  pdf(file.path('out', paste0(name, '.pdf')))
+OutputFile <- function(name, ext, dir='out') {
+  file.path(dir, paste0(name, '.', ext))
+}
+
+PlotWrite <- function(x, name) {
+  pdf(OutputFile(name=name, ext='pdf'))
   fancyRpartPlot(x)
   dev.off()
 }
 
-predictWrite <- function(tree, name, newdata = test) {
-  prediction <- predict(tree, newdata = newdata, type = 'class')
-  submission <- data.frame(PassengerId = test$PassengerId, Survived = prediction)
-  write.csv(submission, file = file.path('out', paste0(name, '.csv')), row.names = FALSE, quote = FALSE)
+PredictWrite <- function(tree, name, newdata=test) {
+  prediction <- predict(tree, newdata=newdata, type='class')
+  submission <- data.frame(PassengerId=test$PassengerId, Survived=prediction)
+  write.csv(submission, file=OutputFile(name=name, ext='csv'), row.names=F, quote=F)
+  prediction
 }
 
 # generate rpart decision tree model with default options
-model <- function(name, equation, data, newdata = test) {
-  tree <- rpart(equation, data = data, method = 'class')
-  pdfPlot(tree, name)
-  prediction <- predictWrite(tree, name, newdata = newdata)
-  list(name = name, tree = tree, prediction = prediction)
+Model <- function(name, equation, data, newdata=test) {
+  tree <- rpart(equation, data=data, method='class')
+  PlotWrite(tree, name)
+  prediction <- PredictWrite(tree, name, newdata=newdata)
+  list(name=name, tree=tree, prediction=prediction)
 }
 
 # specialisations of above with particular equations, i.e. factors specified
-simple.model <- partial(model, equation = Survived ~ Sex + Age + Pclass)
-complex.model <- partial(model, equation = Survived ~ Sex + Age + Pclass + Parch + SibSp + Fare)
+SimpleModel <- partial(Model, equation=(Survived ~ Sex + Age + Pclass))
+ComplexModel <- partial(Model, equation=(Survived ~ Sex + Age + Pclass + Parch + SibSp + Fare))
 
 # load training and test data
 train <- read_csv('train.csv')
@@ -43,14 +48,14 @@ dir.create('out', showWarnings=F)
 # Factors: sex, age, passenger class
 # Imputation: none
 # Kaggle Score: 0.73684
-model.sex.age.pclass <- simple.model(data = train, name = 'model.sex.age.pclass')
+model.sex.age.pclass <- SimpleModel(data=train, name='model.sex.age.pclass')
 
 ## More complex model
 #
 # Factors: sex, age, passenger class, parents/children, siblings/spouses, fare
 # Imputation: none
 # Kaggle Score: 0.79426
-model.sex.age.pclass.parch.sibsp.fare <- complex.model(data = train, name = 'model.sex.age.pclass.parch.sibsp.fare')
+model.sex.age.pclass.parch.sibsp.fare <- ComplexModel(data = train, name = 'model.sex.age.pclass.parch.sibsp.fare')
 
 # training data with average imputed age
 average.age <- mean(train$Age, na.rm = TRUE)
@@ -63,7 +68,7 @@ train.average.age %<>% mutate(Age = ifelse(is.na(Age), average.age, Age))
 # Imputation: age (average on train only)
 # Kaggle Score: 0.73684
 # (exactly the same as non imputed)
-model.sex.age.pclass.impute.average.age.train.only <- simple.model(data = train.average.age, name = 'model.sex.age.pclass-imputed.average.age.train.only')
+model.sex.age.pclass.impute.average.age.train.only <- SimpleModel(data = train.average.age, name = 'model.sex.age.pclass-imputed.average.age.train.only')
 
 # test data with average imputed age
 test.average.age <- test
@@ -75,14 +80,14 @@ test.average.age %<>% mutate(Age = ifelse(is.na(Age), average.age, Age))
 # Imputation: age (average on train and test data)
 # Kaggle Score: 0.73684
 # (exactly the same as non imputed)
-model.sex.age.pclass.impute.average.age <- simple.model(data = train.average.age, newdata = test.average.age, name = 'model.sex.age.pclass-imputed.average.age')
+model.sex.age.pclass.impute.average.age <- SimpleModel(data = train.average.age, newdata = test.average.age, name = 'model.sex.age.pclass-imputed.average.age')
 
 ## Complex model, average imputation
 #
 # Factors: sex, age, passenger class, parents/children, siblings/spouses, fare
 # Imputation: age (average)
 # Kaggle Score: 0.77990
-model.sex.age.pclass.parch.sibsp.fare.impute.average.age <- complex.model(data = train.average.age, newdata = test.average.age, name = 'model.sex.age.pclass.parch.sibsp.fare-imputed.average.age')
+model.sex.age.pclass.parch.sibsp.fare.impute.average.age <- ComplexModel(data = train.average.age, newdata = test.average.age, name = 'model.sex.age.pclass.parch.sibsp.fare-imputed.average.age')
 
 # linear model for age imputation
 age.model <- lm(Age ~ SibSp + Sex + Pclass, train)
@@ -97,7 +102,7 @@ train.lm.age$Age[is.na(train$Age)] = train.age.prediction[is.na(train$Age)]
 # Kaggle score: 0.77033
 test.lm.age <- test
 test.lm.age$Age[is.na(test$Age)] = test.age.prediction[is.na(test$Age)]
-model.sex.age.pclass.parch.sibsp.fare.impute.lm.age <- complex.model(data = train.lm.age, newdata = test.lm.age, name = 'model.sex.age.pclass.parch.sibsp.fare.impute.lm.age')
+model.sex.age.pclass.parch.sibsp.fare.impute.lm.age <- ComplexModel(data = train.lm.age, newdata = test.lm.age, name = 'model.sex.age.pclass.parch.sibsp.fare.impute.lm.age')
 
 # tree4 is not defined -- can't remember what this bit was doing, so commented it out for now
 #Prediction4 <- predict(tree4, newdata=test, type = 'class')
